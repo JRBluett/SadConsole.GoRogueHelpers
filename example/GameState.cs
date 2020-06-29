56ab7870cf8c159b0fea6893784764736da45ba6
JRBluett <jbluett@phynd.com>
2020-06-29 15:01:58 -0500

    Update GameState.cs fix diagnostic code in comment

--- example/GameState.cs
+++ example/GameState.cs
@@ -12,9 +12,9 @@
             Maps.Generators.DoorGenerator.Generate(gen.SadConsoleMap, gen.Rooms, "door", 20);
 
             // Temp SHOW ALL TILES
-            //for (var x = 0; x < gen.SadConsoleMap.Width; x++)
-            //for (var y = 0; y < gen.SadConsoleMap.Height; y++)
-            //    gen.SadConsoleMap[x, y].Flags = SadConsole.Helpers.SetFlag(gen.SadConsoleMap[x, y].Flags, (int)SadConsole.Maps.TileFlags.Seen | (int)SadConsole.Maps.TileFlags.InLOS | (int)SadConsole.Maps.TileFlags.Lighted);
+            //for (int x = 0; x < gen.SadConsoleMap.Width; x++)
+            //    for (int y = 0; y < gen.SadConsoleMap.Height; y++)
+            //        gen.SadConsoleMap.GetTerrain<Tile>(x, y).Flags = SadConsole.Helpers.SetFlag(gen.SadConsoleMap.GetTerrain<Tile>(x, y).Flags, (int)TileFlags.Seen | (int)TileFlags.InLOS | (int)TileFlags.Lighted);
 
             // Create player
             gen.SadConsoleMap.ControlledGameObject = new GameObjects.Player(gen.SadConsoleMap, gen.Rooms[0].InnerRect.Center);
